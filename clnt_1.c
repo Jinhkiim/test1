6b425d1f8ed72fcbfb3ea54e40af1aad930c4ae7
Jinhkiim <admin@MacBook-Air.local>
2013-06-04 00:59:10 +0900

    exercise 1 end

--- clnt_1.c
+++ clnt_1.c
@@ -8,46 +8,39 @@
 void error_handling(char *message);
 
 int main(int argc, char *argv[]) {
-  int serv_sock;
-  int clnt_sock;
+  int sock;
 
   struct sockaddr_in serv_addr;
-  struct sockaddr_in clnt_addr;
-  socklen_t clnt_addr_size;
 
-  char message[] = "hello world!";
+  char message[30];
+  int std_len;
 
-  if (argc != 2)
+  if (argc != 3)
   {
     /* code */
-    printf("Usage : %s <port> \n", argv[0]);
+    printf("Usage : %s <ip> <port> \n", argv[0]);
     exit(1);
   }
 
-  serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-  if (serv_sock == -1)
+  sock = socket(PF_INET, SOCK_STREAM, 0);
+  if (sock == -1)
     error_handling("socket() error");
 
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serv_addr.sin_port=htons(atoi(argv[1]));
+  serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+  serv_addr.sin_port=htons(atoi(argv[2] ));
 
-  if (bind(serv_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1)
-    error_handling("bind() error");
+  if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1)
+    error_handling("connect() error");
 
-  if (listen(serv_sock, 5) == -1)
-    error_handling("listen() error");
+  read(sock, message, sizeof(message));
+  if(std_len == -1)
+    error_handling("read() error");
 
-  clnt_addr_size = sizeof(clnt_addr);
-  clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+  printf("Message from server : %s\n", message);
 
-  if (clnt_sock == -1)
-    error_handling("accept() error");
-
-  write(clnt_sock, message, sizeof(message));
-  close(clnt_sock);
-  close(serv_sock);
+  close(sock);
   
   return 0;
 } 
